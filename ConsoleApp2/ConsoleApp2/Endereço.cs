883ecc51caf194e00a69682814567f8e67277683
Otávio <101956836+OtavioVB@users.noreply.github.com>
2022-06-19 15:32:06 -0300

    Add files via upload

---
+++ ConsoleApp2/ConsoleApp2/Endereço.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp2
+{
+    public partial class Endereço
+    {
+        public string Cep { get; set; }
+        public string Logradouro { get; set; }
+        public string Complemento { get; set; }
+        public string Bairro { get; set; }
+        public string Localidade { get; set; }
+        public string Uf { get; set; }
+        public long Ibge { get; set; }
+        public long Gia { get; set; }
+        public long Ddd { get; set; }
+        public long Siafi { get; set; }
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        async static Task Main(string[] args)
+        {
+            Console.WriteLine("Insira o CEP");
+            HttpClient client = new HttpClient { BaseAddress = new Uri("https://viacep.com.br/ws/") };
+            string Retorno = Console.ReadLine();
+            HttpResponseMessage response = await client.GetAsync(Retorno + "/json/");
+            string content = await response.Content.ReadAsStringAsync();
+            Endereço jsonConvert = JsonConvert.DeserializeObject<Endereço>(content);
+            Console.WriteLine(jsonConvert.Localidade);
+            Console.ReadLine();
+        }
+    }
+}
